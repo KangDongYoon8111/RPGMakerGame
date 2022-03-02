1287be68970944f2340b228485cf426188072496
강동윤 <davidkang8111@gmail.com>
2022-03-02 10:21:41 +0900

    20220302

---
+++ RPGMakerGame/Assets/02.Scripts/IncodingTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncodingTest : MonoBehaviour
+{
+    // 한글 인코딩 테스트1
+    // 한글 인코딩 테스트2
+    // Korean Incoding Test3
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
